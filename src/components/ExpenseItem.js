03a7972d2ee9ff2c4f30f81e65736c3219d06ddd
Vicky Kumar <vickykumarjsr@gmail.com>
2022-08-06 21:26:04 +0530

    initial commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <div>March 28 2022</div>
+      <div>
+        <h2>Car Insurance</h2>
+        <div>$297.4</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
