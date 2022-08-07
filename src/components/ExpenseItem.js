8d0dfd84ce1ea3fed4d95d3dc4f43d65267dd7f0
Vicky Kumar <vickykumarjsr@gmail.com>
2022-08-07 11:33:14 +0530

    day one commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,13 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
   return (
-    <div>
-      <div>March 28 2022</div>
-      <div>
-        <h2>Car Insurance</h2>
-        <div>$297.4</div>
+    <div className='expense-item'>
+      <ExpenseDate date={props.date} />
+      <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
